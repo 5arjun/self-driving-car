9f21cacd779b195766b12c0f30b1a85503b65b7e
Arjun Patel <arjunpat107@gmail.com>
2025-08-17 19:04:49 -0400

    gave the middle lanes dashes

--- road.js
+++ road.js
@@ -18,6 +18,11 @@ class Road {
 
     for (let i = 0; i <= this.laneCount; i++) {
       const x = lerp(this.left, this.right, i / this.laneCount);
+      if (i > 0 && i < this.laneCount) {
+        ctx.setLineDash([20, 20]);
+      }else{
+        ctx.setLineDash([]);
+      }
       ctx.beginPath();
       ctx.moveTo(x, this.top);
       ctx.lineTo(x, this.bottom);
@@ -25,4 +30,3 @@ class Road {
     }
   }
 }
-
