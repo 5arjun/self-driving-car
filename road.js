822851d5b1f164ebe1310756430bbf7b4df6d537
Arjun Patel <arjunpat107@gmail.com>
2025-08-17 18:56:41 -0400

    lerp function for lanes

--- road.js
+++ road.js
@@ -16,6 +16,13 @@ class Road {
     ctx.lineWidth = 5;
     ctx.strokeStyle = "white";
 
+    for(let i = 0; i <= this.laneCount; i++){
+        const x = lerp(
+            this.left,
+            this.right,
+            i / this.laneCount
+        );
+    }
     ctx.beginPath();
     ctx.moveTo(this.left, this.top);
     ctx.lineTo(this.left, this.bottom);
@@ -25,6 +32,10 @@ class Road {
     ctx.moveTo(this.right, this.top);
     ctx.lineTo(this.right, this.bottom);
     ctx.stroke();
-    
+
   }
+}
+
+function lerp(A,B,t){
+    return A + (B - A) * t;
 }
